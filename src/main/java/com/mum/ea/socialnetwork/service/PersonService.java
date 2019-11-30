672708327a04a94ffa4926483e9124f7f29c8a6a
MUNEZERO EMMANUEL <emmy@MUNEZEROs-MacBook-Pro.local>
2019-11-30 12:19:20 -0600

    I added Person and address domain and controller

---
+++ src/main/java/com/mum/ea/socialnetwork/service/PersonService.java
@@ -0,0 +1,13 @@
+package com.mum.ea.socialnetwork.service;
+
+import com.mum.ea.socialnetwork.domain.Person;
+
+import java.util.List;
+
+public interface PersonService {
+    public void addPerson(Person person);
+    public Person getPersonById(Long id);
+    public List<Person> getAllPerson();
+
+
+}
---
+++ src/main/java/com/mum/ea/socialnetwork/serviceImpl/PersonServiceImpl.java
@@ -0,0 +1,33 @@
+package com.mum.ea.socialnetwork.serviceImpl;
+
+import com.mum.ea.socialnetwork.domain.Person;
+import com.mum.ea.socialnetwork.repository.PersonRepository;
+import com.mum.ea.socialnetwork.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Service
+@Transactional
+public class PersonServiceImpl implements PersonService {
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Override
+    public void addPerson(Person person) {
+
+        personRepository.save(person);
+
+    }
+
+    @Override
+    public Person getPersonById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Person> getAllPerson() {
+        return (List<Person>) personRepository.findAll();
+    }
+}
