656be84e4cd662d8748e4242d3e5392f85f2ed9b
dfantaw <dfantaw@gmail.com>
2019-11-30 16:59:07 -0600

    add user_relation service, repo, and controller

---
+++ src/main/java/com/mum/ea/socialnetwork/service/user_relationService.java
@@ -0,0 +1,8 @@
+package com.mum.ea.socialnetwork.service;
+
+
+import com.mum.ea.socialnetwork.domain.user_relation;
+
+public interface user_relationService {
+    public void save(user_relation userRelation);
+}
